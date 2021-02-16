84012a448ef5cc08a7113db92240ea996346aa5f
Stevie IsMagic <stevieismagic@gmail.com>
2021-02-15 21:11:47 -0800

    return list of noted for the provided userId

---
+++ list.js
@@ -0,0 +1,20 @@
+import handler from "./libs/handler-lib";
+import dynamoDb from "./libs/dynamodb-lib";
+
+export const main = handler(async (event, context) => {
+    const params = {
+        TableName: process.env.tableName,
+        // KeyConditionExpression defines the condition for the query
+        // userId = :userId means only return items matching the 'userId' partition key
+        KeyConditionExpression: "userId = :userId",
+        // ExpressionAttributeValues defines the value in the condition
+        ExpressionAttributeValues: {
+            ":userId": "123",
+        },
+    };
+
+    const result = await dynamoDb.query(params);
+
+    // return the matching list items in response body
+    return result.Items;
+});
